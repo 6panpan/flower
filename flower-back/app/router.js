fe72f7d3d36bd0e866c9118ce089d23c6452c257
weixiao123456789 <2650800974@qq.com>
2020-09-22 11:12:03 +0800

    cr更新笔记

--- flower-back/app/router.js
+++ flower-back/app/router.js
@@ -5,12 +5,10 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-<<<<<<< HEAD
   // router.get('/', controller.home.index);
   // router.post('/',controller)
   
-};
-=======
   router.get('/flowerByPurpose.do', controller.flowerController.flowerByPurpose);
-}; 
->>>>>>> aff434c1597d3c302a8ac74677f3903c28b67dd9
+};
+
+
