2628341f2106e1839fa66167ea4deba075bd5850
1683605792 <1683605792@qq.com>
2020-09-22 10:12:00 +0800

    个人中心导航

---
+++ flower-front/src/components/person/content/Birthset.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Order extends React.Component{
+    // constructor(){
+    //     super()
+        
+    // }
+    render(){
+        return (
+            <>
+        <div>
+        生日纪念日设置
+
+        </div>
+        </>
+        )
+        
+    }
+}
